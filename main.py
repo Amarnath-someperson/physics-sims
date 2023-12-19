8c6bcb70c0ced13fe5065b31cff8ef1dd4f06ee2
Amarnath K <amarnathkunnummal112@gmail.com>
2023-12-19 19:20:05 +0530

    idk

--- main.py
+++ main.py
@@ -10,22 +10,30 @@ width = screen.get_width()
 height = screen.get_height()
 fps = 60
 WHITE = (255, 255, 255)
-font = pygame.font.SysFont('Arial', 24)
+
+font = pygame.font.SysFont('Corbel', 24)
+head = pygame.font.SysFont('Corbel', 60).render('Menu', False, WHITE)
 text = font.render('Particle Collider' , False , WHITE) 
-box = text.get_width()
+box = text.get_size()
+
+select=False
 while True:
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()  # ends all code, safer than break
             
         if event.type == pygame.MOUSEBUTTONDOWN:
-            pass
+            if mouse:
+                select = True
+                pass
             
             
     mouse = pygame.mouse.get_pos()
-    screen.blit(text, (width//2-box//2, height//2))        
-    
+    if select:
+        screen.fill((20, 80, 170))
+    if not select:
+        screen.blit(head, (width//2-head.get_width()//2, height//2-head.get_height()//2-100))
+        screen.blit(text, (width//2-box[0]//2, height//2-box[1]//2))        
     pygame.display.update()
     pygame.time.Clock().tick(fps)
